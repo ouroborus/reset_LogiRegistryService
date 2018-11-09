aa6e9c536aa47365c84dd5f9e61f6c712a60414c
ouroborus <deadchicken@gmail.com>
2018-11-08 21:55:40 -0800

    initial commit

---
+++ reset-LogiRegistryService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceProcess;
+
+namespace reset_LogiRegistryService {
+    class Program {
+        static int Main(string[] args) {
+            ServiceController serviceController = new ServiceController("LogiRegistryService");
+            try {
+                if (serviceController.Status.Equals(ServiceControllerStatus.Running)) {
+                    serviceController.Stop();
+                    serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
+                    serviceController.Start();
+                    serviceController.WaitForStatus(ServiceControllerStatus.Running);
+                }
+            }
+            catch {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
